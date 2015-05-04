7d1fd7b4cd2beef2d0f86606a9917cc5beec59e4
Ofer Livny <ofer@Ofers-MacBook-Pro.local>
2015-05-04 12:33:41 +0300

    Initial Commit

---
+++ ioscv/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ioscv
+//
+//  Created by Ofer Livny on 5/4/15.
+//  Copyright (c) 2015 Ofer Livny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ioscv/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ioscv
+//
+//  Created by Ofer Livny on 5/4/15.
+//  Copyright (c) 2015 Ofer Livny. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
